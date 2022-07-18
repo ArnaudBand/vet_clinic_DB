467555909085485e4481c4d96451c5ddb1e4f9c7
Arnaud <bandonkeyea@gmail.com>
2022-07-18 16:39:40 -0600

    create anmals table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+create table animals (
+    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name varchar(50),
+    date_of_birth date,
+    escape_attempts int,
+    neutered boolean,
+    weight_kg decimal
+)
