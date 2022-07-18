ef9f186754a679a77abbe6c26ec63e8447d748cf
Arnaud <bandonkeyea@gmail.com>
2022-07-18 16:38:35 -0600

    Create animals table

--- data.sql
+++ data.sql
@@ -1,5 +1,7 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+insert into animals (name, date_of_birth, escape_attempts, neutered, weight_kg) values
+        ('Agumon', 'Feb 3, 2020', 0, true, 10.23),
+        ('Gabumon', 'Nov 15, 2018', 2, true, 8),
+        ('Pikachu', 'Jan 7, 2021', 1, false, 15.04),
+        ('Devimon', 'May 12, 2017', 5, true, 11);
