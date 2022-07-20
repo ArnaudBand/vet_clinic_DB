6ced5a33825c67938f30a47898496dab93b99bef
Arnaud <bandonkeyea@gmail.com>
2022-07-20 13:49:25 -0600

    Create 2 tables and update column on animals table

--- schema.sql
+++ schema.sql
@@ -7,6 +7,24 @@ create table animals (
     escape_attempts int,
     neutered boolean,
     weight_kg decimal
-)
+);
 
 alter table animals add species varchar(50);
+
+create table owners(
+    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    full_name varchar(50),
+    age int 
+);
+
+create table species(
+    id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name varchar(50)
+);
+
+alter table animals add column species_id int;
+ALTER TABLE animals ADD CONSTRAINT id_species FOREIGN KEY (species_id) REFERENCES species (id);
+alter table animals add column owner_id int;
+ALTER TABLE animals ADD CONSTRAINT id_owners FOREIGN KEY (owner_id) REFERENCES owners (id);
+
+
