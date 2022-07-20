a7e182c817d36910ad901a73bb171c8dcd56afa5
Arnaud <bandonkeyea@gmail.com>
2022-07-20 07:48:03 -0600

    Remove update and delete

--- schema.sql
+++ schema.sql
@@ -10,14 +10,3 @@ create table animals (
 )
 
 alter table animals add species varchar(50);
-
-update animals set species = 'unspecified';
-
-update animals set species = 'digimon' where name like '%mom';
-
-update animals set species = 'pokemon' where species is null;
-
-delete from animals;
-delete from animals where date_of_birth > date '2022-01-01';
-update animals set weight_kg = weight_kg * -1;
-update animals set weight_kg = weight_kg * -1 where weight_kg < 0;
